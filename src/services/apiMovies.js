ea5932f4abd9fdd777bf1e00f11992235b8911fc
Atopovy <mpanek138@gmail.com>
2023-09-12 14:47:12 +0200

    updated api urls

--- src/services/apiMovies.js
+++ src/services/apiMovies.js
@@ -1,7 +1,7 @@
 const KEY = "c573a666";
 
 export async function getMovies(query) {
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
   const data = await res.json();
   return data;
 }
