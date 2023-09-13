dd49cd2a321b11a4e9dc4a7c41e1f1559d77a33a
Atopovy <mpanek138@gmail.com>
2023-09-13 12:25:15 +0200

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter } from "react-router-dom";
 import AppLayout from "./pages/AppLayout";
 import Homepage from "./pages/Homepage";
 import Search from "./pages/Search";
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MovieDetailsProvider } from "./context/MovieListContext";
 import MoviesToWatch from "./pages/MoviesToWatch";
 import WatchedMovies from "./pages/WatchedMovies";
+import NotFound from "./NotFound";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -44,7 +45,14 @@ function App() {
   return (
     <MovieDetailsProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <RouterProvider router={router}>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/search" component={Search} />
+          <Route path="/watchlist" component={MoviesToWatch} />
+          <Route path="/watched" component={WatchedMovies} />
+          {/* Add a catch-all route for 404 errors */}
+          <Route component={NotFound} />
+        </RouterProvider>
       </QueryClientProvider>
     </MovieDetailsProvider>
   );
